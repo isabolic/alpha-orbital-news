a2b43642ceb5fb34efa31de9e57f2cb059013397
isabolic <ivan.sabolic@mantro.hr>
2022-10-12 09:54:06 +0200

    integrated react-query

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,8 +1,28 @@
-import '../styles/globals.css'
-import type { AppProps } from 'next/app'
+import React, { useState } from "react";
+import "../styles/globals.css";
+import type { AppProps } from "next/app";
+import { AppLayout } from "../components";
+import {
+  Hydrate,
+  QueryClient,
+  QueryClientProvider,
+} from "@tanstack/react-query";
+import type { DehydratedState } from "@tanstack/react-query";
 
-function MyApp({ Component, pageProps }: AppProps) {
-  return <Component {...pageProps} />
+function MyApp({
+  Component,
+  pageProps,
+}: AppProps<{ dehydratedState: DehydratedState }>) {
+  const [queryClient] = useState(() => new QueryClient());
+  return (
+    <QueryClientProvider client={queryClient}>
+      <Hydrate state={pageProps.dehydratedState}>
+        <AppLayout>
+          <Component {...pageProps} />
+        </AppLayout>
+      </Hydrate>
+    </QueryClientProvider>
+  );
 }
 
-export default MyApp
+export default MyApp;

78e1d4fa30c74a0b42194ea8d8fdfeb0b0b4ad91
isabolic <ivan.sabolic@mantro.hr>
2022-10-12 17:54:55 +0200

    integrated react query dev tools

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,4 +1,4 @@
-import React, { useState } from "react";
+import React, { lazy, useRef } from "react";
 import "../styles/globals.css";
 import type { AppProps } from "next/app";
 import { AppLayout } from "../components";
@@ -8,21 +8,44 @@ import {
   QueryClientProvider,
 } from "@tanstack/react-query";
 import type { DehydratedState } from "@tanstack/react-query";
+import { ReactQueryDevtools } from "@tanstack/react-query-devtools";
 
-function MyApp({
+const ReactQueryDevtoolsProduction = lazy(() =>
+  import("@tanstack/react-query-devtools/build/lib/index.prod.js").then(
+    (d) => ({
+      default: d.ReactQueryDevtools,
+    })
+  )
+);
+
+const MyApp = ({
   Component,
   pageProps,
-}: AppProps<{ dehydratedState: DehydratedState }>) {
-  const [queryClient] = useState(() => new QueryClient());
+}: AppProps<{ dehydratedState: DehydratedState }>) => {
+  const [showDevtools, setShowDevtools] = React.useState(false);
+
+  React.useEffect(() => {
+    // @ts-ignore
+    window.toggleDevtools = () => setShowDevtools((old) => !old);
+  }, []);
+
+  const [queryClient] = React.useState(() => new QueryClient());
+
   return (
     <QueryClientProvider client={queryClient}>
       <Hydrate state={pageProps.dehydratedState}>
         <AppLayout>
           <Component {...pageProps} />
         </AppLayout>
+        <ReactQueryDevtools initialIsOpen />
+        {showDevtools && (
+          <React.Suspense fallback={null}>
+            <ReactQueryDevtoolsProduction />
+          </React.Suspense>
+        )}
       </Hydrate>
     </QueryClientProvider>
   );
-}
+};
 
 export default MyApp;

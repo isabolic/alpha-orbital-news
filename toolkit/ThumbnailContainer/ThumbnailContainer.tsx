6cbb73a1a1330227db2ae99b58fac3bc68a92435
isabolic <ivan.sabolic@mantro.hr>
2022-10-12 13:40:53 +0200

    implemented ThumbnailContainer

---
+++ toolkit/ThumbnailContainer/ThumbnailContainer.tsx
@@ -0,0 +1,9 @@
+import { styled } from "@stitches/react";
+
+const ThumbnailContainer = styled("div", {
+  position: "relative",
+  width: "300px",
+  height: "300px",
+});
+
+export { ThumbnailContainer };

9368fa14e857ebc57c3a36b22b703723ba618f4e
isabolic <ivan.sabolic@mantro.hr>
2022-10-12 09:52:16 +0200

    news dto

---
+++ dto/news.ts
@@ -0,0 +1,11 @@
+interface NewsArticle {
+  title: string;
+  slug: string;
+  date: string;
+  excerpt: string;
+  post_image: string;
+  post_thumbnail: string;
+  post_category_id: string;
+}
+
+export type { NewsArticle };

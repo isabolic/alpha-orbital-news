3673e005da3fd43dedc8e5e380806cb5ce0addac
isabolic <ivan.sabolic@mantro.hr>
2022-10-12 17:55:09 +0200

    sm refactor

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,20 +1,20 @@
 import { QueryClient, dehydrate } from "@tanstack/react-query";
-import type { NextPage } from "next";
+import type { GetStaticProps, NextPage } from "next";
 import React from "react";
 import { NewsArticleList } from "@components";
 import { fetchNews } from "../hooks";
+import { NewsArticle } from "../dto/NewsArticle";
 
-export async function getStaticProps() {
+export const getStaticProps: GetStaticProps = async () => {
   const queryClient = new QueryClient();
-
-  await queryClient.prefetchQuery(["news"], () => fetchNews());
+  await queryClient.prefetchQuery<NewsArticle>(["news"], fetchNews);
 
   return {
     props: {
       dehydratedState: dehydrate(queryClient),
     },
   };
-}
+};
 
 const Main: NextPage = () => {
   return (

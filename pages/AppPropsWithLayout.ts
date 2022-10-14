2e23a3eadc0a64ac264944f8478eba91a3e6e6a0
isabolic <ivan.sabolic@mantro.hr>
2022-10-14 15:47:38 +0200

    org type export

--- pages/AppPropsWithLayout.ts
+++ pages/AppPropsWithLayout.ts
@@ -1,6 +1,8 @@
 import { AppProps } from "next/app";
 import { NextPageWithLayout } from "./NextPageWithLayout";
 
-export type AppPropsWithLayout<P = {}> = AppProps<P> & {
+type AppPropsWithLayout<P = {}> = AppProps<P> & {
   Component: NextPageWithLayout<P>;
 };
+
+export type { AppPropsWithLayout };

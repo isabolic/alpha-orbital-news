58227b3f60eea0bb192b804494ed8ebafcd4c84c
isabolic <ivan.sabolic@mantro.hr>
2022-10-14 15:46:44 +0200

    rm hydrate state

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,36 +1,18 @@
-import React, { lazy } from "react";
+import React from "react";
 import "../styles/globals.css";
-import {
-  Hydrate,
-  QueryClient,
-  QueryClientProvider,
-} from "@tanstack/react-query";
-import type { DehydratedState } from "@tanstack/react-query";
+import { QueryClient, QueryClientProvider } from "@tanstack/react-query";
 import { ReactQueryDevtools } from "@tanstack/react-query-devtools";
 import { AppPropsWithLayout } from "./AppPropsWithLayout";
 
-const ReactQueryDevtoolsProduction = lazy(() =>
-  import("@tanstack/react-query-devtools/build/lib/index.prod.js").then(
-    (d) => ({
-      default: d.ReactQueryDevtools,
-    })
-  )
-);
-
-const MyApp = ({
-  Component,
-  pageProps,
-}: AppPropsWithLayout<{ dehydratedState: DehydratedState }>) => {
+const MyApp = ({ Component, pageProps }: AppPropsWithLayout) => {
   const [queryClient] = React.useState(() => new QueryClient());
 
   const getLayout = Component.getLayout ?? ((page) => page);
 
   return (
     <QueryClientProvider client={queryClient}>
-      <Hydrate state={pageProps.dehydratedState}>
-        {getLayout(<Component {...pageProps} />)}
-        <ReactQueryDevtools initialIsOpen />
-      </Hydrate>
+      {getLayout(<Component {...pageProps} />)}
+      <ReactQueryDevtools initialIsOpen />
     </QueryClientProvider>
   );
 };

bb6b332a8f5c845cc8959c7914ed0b823c6e08c6
isabolic <ivan.sabolic@mantro.hr>
2022-10-15 09:27:40 +0200

    [WIP] integrated func. to delete articles by category

--- components/NewsArticleContainer/NewsStatusBar.tsx
+++ components/NewsArticleContainer/NewsStatusBar.tsx
@@ -1,6 +1,12 @@
-import { useNewsArticleRefresh } from "@hooks";
+import {
+  useDeleteNewsArticleByCategory,
+  useNewsArticleCategory,
+  useNewsArticleRefresh,
+} from "@hooks";
 import { css } from "@stitches/react";
-import { Button } from "@toolkit";
+import { Button, Select, Switch } from "@toolkit";
+import { CategoryType, getCategoyLabel } from "@utils";
+import { useState } from "react";
 
 interface NewsStatusBarProps {
   numberOfArticles: number;
@@ -10,7 +16,7 @@ interface NewsStatusBarProps {
 const container = css({
   display: "flex",
   alignItems: "center",
-  justifyContent: "end",
+  justifyContent: "space-between",
   minHeight: "2rem",
 });
 
@@ -20,24 +26,83 @@ const button = css({
   borderRadius: "4px",
 });
 
+const box = css({
+  display: "flex",
+  alignItems: "center",
+});
+
+const space = css({
+  padding: "0px 5px",
+});
+
+const fadeOut = css({
+  visibility: "visible",
+  opacity: 0,
+  transition: "visibility 0s linear 0s, opacity 300ms",
+});
+
+const fadeIn = css({
+  visibility: "visible",
+  opacity: 1,
+  transition: "visibility 0s linear 0s, opacity 300ms",
+});
+
 const NewsStatusBar = ({
   numberOfArticles,
   showRefetch,
 }: NewsStatusBarProps) => {
+  const [showMenu, setShowMenu] = useState(false);
+  const [selectedCategory, setSelectedCategory] =
+    useState<null | CategoryType>();
+
   const { mutate } = useNewsArticleRefresh();
+  const { mutate: mutateDeleteByCategory } = useDeleteNewsArticleByCategory();
+  const { data } = useNewsArticleCategory();
 
   const onRefetchHandler = () => {
     mutate();
   };
 
+  const deleteNewsArticleByCategoryHandler = () => {
+    mutateDeleteByCategory(selectedCategory!);
+  };
+
+  const categoryList = data?.map((x) => ({
+    value: x,
+    label: getCategoyLabel(x),
+  }));
+
   return (
     <div className={container()}>
-      Currently showing {numberOfArticles} articles
-      {showRefetch && (
-        <Button className={button()} onClick={onRefetchHandler}>
-          Refetch
-        </Button>
-      )}
+      <div className={box()}>
+        <Switch onChange={setShowMenu} />
+        <div className={showMenu ? fadeIn() : fadeOut()}>
+          <div className={box()}>
+            <div className={space()}>
+              <Select<CategoryType>
+                onChange={setSelectedCategory}
+                list={categoryList ?? []}
+              />
+            </div>
+            <div className={space()}>
+              <Button
+                disabled={!selectedCategory}
+                onClick={deleteNewsArticleByCategoryHandler}
+              >
+                Delete
+              </Button>
+            </div>
+          </div>
+        </div>
+      </div>
+      <div>
+        Currently showing {numberOfArticles} articles
+        {showRefetch && (
+          <Button className={button()} onClick={onRefetchHandler}>
+            Refetch
+          </Button>
+        )}
+      </div>
     </div>
   );
 };
